27aaf193a5e86104db44d7ee73a61ca714a1b35d
CarlosGzz <carlosgzz93@gmail.com>
2016-04-27 16:40:17 -0500

    Carlos Gonzalez
    cambio a conexion

--- MODEL/connect.php
+++ MODEL/connect.php
@@ -1,8 +1,8 @@
 <?php
-	$servername = "localhost";
-	$username = "treats";
-	$password = "treats1";
-	$dbname = "Treats";
+	$servername = "us-cdbr-iron-east-03.cleardb.net";
+	$username = "b0cd0f01775543";
+	$password = "b5e22ee3";
+	$dbname = "heroku_513fc836dde6412";
 
 	// Create connection
 	$db = new mysqli($servername, $username, $password, $dbname);
