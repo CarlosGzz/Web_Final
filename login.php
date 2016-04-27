688716aa29d6d6bc06e08ebc346793776877f940
rcalde <raul_alejandro73@hotmail.com>
2016-04-27 16:54:11 -0500

    Raúl Alejandro Calderón Garza
    Vista del login

---
+++ login.php
@@ -0,0 +1,60 @@
+<html>
+<head>
+	<title></title>
+	 <!-- Compiled and minified CSS -->
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.97.6/css/materialize.min.css">
+  <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+
+  <!-- Compiled and minified JavaScript -->
+  
+
+          
+</head>
+<link type="text/css" rel="stylesheet" href="css/label.css"  media="screen,projection"/>
+<body>	
+
+	<nav>
+    	<div class="nav-wrapper">
+     	 <a href="#" class="brand-logo">Logo</a>
+     	 <ul id="nav-mobile" class="right hide-on-med-and-down">
+      	  <li><a href="sass.html">Mis Eventos</a></li>
+      	  <li><a href="badges.html">Eventos</a></li>
+      	  <li><a href="collapsible.html">Amigos</a></li>
+      	 </ul>
+   		</div>
+  	</nav>
+	
+	<div class="container">
+		<h1>Login</h1>
+	    <div class="row">
+        	<div class="input-field col s12">
+          		<input id="email" type="email" class="validate"><!-- campor para ingresar el Email -->
+          		<label for="email">Email</label>
+        	</div>
+    	</div>
+    	<div class="row">
+        	<div class="input-field col s12">
+          		<input id="password" type="password" class="validate"><!-- campor para ingresar el contrasena -->
+          		<label for="password">Password</label>
+        	</div>
+    	</div>
+    	<div class="row">
+			<button class="btn waves-effect waves-light" type="submit" name="action">Submit
+	    	<i class="material-icons right">send</i>
+	  		</button>
+		</div>
+      
+    </form>
+  </div>
+
+</div>
+	
+<script type="text/javascript" src="https://code.jquery.com/jquery-2.1.1.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.97.6/js/materialize.min.js"></script>
+  <!-- Compiled and minified Jquery -->
+	
+</body>
+</html>
+<?php
+	
+?>
