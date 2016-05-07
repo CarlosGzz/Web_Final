4265c4c3849cf00d18d9ef1f9dca59f14dce62a8
Rodolfo Saldivar <rodomix94@hotmail.com>
2016-05-07 18:58:26 -0500

    i

--- Vista/login-signup.php
+++ Vista/login-signup.php
@@ -34,7 +34,7 @@
   	<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.0/jquery.min.js"></script>
 
 </head>
-<body>dtheryjw
+<body>dtheryibib
 	<!-- NAVBAR -->
 	<nav>
 	    <div class="nav-wrapper">
