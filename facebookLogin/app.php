0d60e20e688039e0a76b808e57fb7273a42bae91
CarlosGzz <carlosgzz93@gmail.com>
2016-05-12 18:02:59 -0500

    debug 4
    asdfasd

--- facebookLogin/app.php
+++ facebookLogin/app.php
@@ -42,13 +42,13 @@
 	}
 
 	$helper = $fb->getRedirectLoginHelper();
-	echo "que pedo1";
+	echo "que pedo1 ";
 	$permissions = ['email']; // optional
-	echo "que pedo2";
-	$callback    = 'http://partydog.herokuapp.com/facebookLogin/login-callback.php';
-	echo "que pedo3";
-	$loginUrl    = $helper->getLoginUrl($callback, $permissions);
-	echo "que pedo";
+	echo "que pedo2 ";
+	$callback = 'http://partydog.herokuapp.com/facebookLogin/login-callback.php';
+	echo "que pedo3 ";
+	$loginUrl = $helper->getLoginUrl($callback, $permissions);
+	echo "que pedo4";
 
 	echo '<a href="' . $loginUrl . '">Log in with Facebook!</a>';
 
